2abbc1d7e96fe2401e868c152e9309641a78bc0f
Octavian <octavian@octavian-HP-G62-Notebook-PC.(none)>
2013-11-11 01:31:30 +0200

    fake php

---
+++ index.php
@@ -0,0 +1,310 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name ="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Home Romania</title>
+		<link rel="stylesheet" href="files/css/bootstrap.css" media="screen" />
+		<link rel="stylesheet" href="files/css/mystyles.css"  media="screen" />
+<!--		<style type="text/css">-->
+<!--			body{-->
+<!--						background-image:url("images/background.jpg");-->
+<!--						background-repeat:no-repeat;-->
+<!--						background-attachment:fixed;-->
+<!--					}-->
+<!--		</style>-->
+	</head>
+	<body id="home">
+
+		<div class = "container">
+
+			<section class="content row">
+<!--				<div class="col-lg-12">-->
+					<header class="clearfix">
+						<section id="branding">
+							<a href="index.html"><img src="images/romania-branding.png" alt="Logo for Romania" style="max-width:100%"></a>
+						</section>
+						<div class = "navbar navbar-default navbar-static-top navbar-custom">
+							<div class = "container">
+
+											<button class = "navbar-toggle" data-toggle = "collapse" data-target = ".navHeaderCollapse" >
+													<span class = "icon-bar"></span>
+													<span class = "icon-bar"></span>
+													<span class = "icon-bar"></span>
+											</button>
+
+											<div class = "collapse navbar-collapse navHeaderCollapse">
+
+														<ul class = "nav navbar-nav ">
+
+														<li class = "dropdown">
+
+																	<a href="index.html">Home</a>
+
+
+															</li>
+
+															<li class = "dropdown">
+
+																	<a href="about_romania.html" class = "dropdown-toggle" >About Romania</a>
+																	<ul class = "dropdown-menu">
+																		<li><a href= "#">Location</a></li>
+																		<li><a href= "#">History</a></li>
+																		<li><a href= "#">Culture</a></li>
+																		<li><a href= "#">People</a></li>
+																	</ul>
+
+															</li>
+
+															<li class = "dropdown">
+
+																	<a href="what_to_do.html" class = "dropdown-toggle" >What to do</a>
+																	<ul class = "dropdown-menu">
+																		<li><a href= "#">Adventure & Activities </a></li>
+																		<li><a href= "#">Birdwatching</a></li>
+
+																	</ul>
+
+															</li>
+
+															<li class = "dropdown">
+
+																	<a href="destinations.html" class = "dropdown-toggle" >Destinations</a>
+																	<ul class = "dropdown-menu">
+																		<li><a href= "#">Top Destinations</a></li>
+																		<li><a href= "#">Black Sea</a></li>
+																		<li><a href= "#">Danube Delta</a></li>
+																		<li><a href= "#">The Painted Monasteries</a></li>
+																		<li><a href= "#">The Carpathian Mountains</a></li>
+																		<li><a href= "#">Medieval Towns</a></li>
+																		<li><a href= "#">Castles & Fortresses</a></li>
+																	</ul>
+
+															</li>
+
+															<li class = "dropdown">
+
+																	<a href="multimedia.html" class = "dropdown-toggle" >Multimedia</a>
+																	<ul class = "dropdown-menu">
+																		<li><a href= "#">Images</a></li>
+																		<li><a href= "#">Videos</a></li>
+																		<li><a href= "#">Download Images</a></li>
+																	</ul>
+
+															</li>
+
+															<li class = "dropdown">
+
+																	<a href="special_interest.html" class = "dropdown-toggle" >Special Interest</a>
+																	<ul class = "dropdown-menu">
+																		<li><a href= "#">Images</a></li>
+																		<li><a href= "#">Videos</a></li>
+																		<li><a href= "#">Download Images</a></li>
+																	</ul>
+
+															</li>
+
+															<li class = "dropdown">
+
+																	<a href="trip_planner.html" class = "dropdown-toggle" >Trip Planner</a>
+																	<ul class = "dropdown-menu">
+																		<li><a href= "#">Getting here and around</a></li>
+																		<li><a href= "#">Transportation</a></li>
+																		<li><a href= "#">Itinerary Ideas</a></li>
+																		<li><a href= "#">Practical Information</a></li>
+																		<li><a href= "#">Distinctive Accommodations</a></li>
+																		<li><a href= "#">Entry Requirements</a></li>
+
+																	</ul>
+
+															</li>
+
+														</ul>
+
+
+											</div>  <!--collapse navbar-collapse navHeaderCollapse	-->
+									</div>  <!--container-->
+						</div>  <!--navbar navbar-inverse navbar-static-top -->
+
+					</header> <!--clearfix-->
+
+
+<!--				</div><!--column-->
+
+				<div id="myCarousel" class="carousel slide">
+
+					<ol class="carousel-indicators">
+						<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+						<li data-target="#myCarousel" data-slide-to="1" ></li>
+						<li data-target="#myCarousel" data-slide-to="2" ></li>
+						<li data-target="#myCarousel" data-slide-to="3" ></li>
+
+					</ol><!--carousel-indicators-->
+
+					<section class="carousel-inner">
+						<div class="active item" >
+							<img  src="images/home images/Delta-Dunarii2.jpg" alt="Photo of Delta Dunarii">
+
+							<div class="carousel-caption">
+								<h3>Danube Delta</h3>
+								<p>A UNESCO Natural World Heritage Site and The place of many wild birds </p>
+							</div><!--carousel-caption-->
+						</div>
+						<div class="item">
+							<img src="images/home images/Drum-Transfagarasanu.jpg" alt="Photo of Drum Transfagarasanu">
+
+							<div class="carousel-caption">
+								<h3>Transfagarasanu Road</h3>
+								<p>Some people say that it is the best road in the world</p>
+							</div><!--carousel-caption-->
+						</div>
+						<div class="item">
+							<img src="images/home images/Lacul-Rosu.jpg" alt="Photo of Lacul Rosu">
+
+							<div class="carousel-caption">
+								<h3>Red Lake</h3>
+								<p>An amazing lake between mountains</p>
+							</div><!--carousel-caption-->
+						</div>
+						<div class="item">
+							<img src="images/home images/Manastirea-Sucevita.jpg" alt="Photo of Manastirea Sucevita">
+
+							<div class="carousel-caption">
+								<h3>Sucevita Monastery</h3>
+								<p>Another UNESCO World Heritage Site with famous Moldavian style of exterior paintings</p>
+							</div><!--carousel-caption-->
+						</div>
+					</section><!--carousel-inner-->
+
+					<a href="#myCarousel" class="left carousel-control" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+					<a href="#myCarousel" class="right carousel-control" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+
+				</div><!--myCarousel-->
+
+				<div class="main col col-lg-7">
+							<h2>Welcome to Romania Tourism!</h2>
+							<p>This website is intended to assist travelers who are planning to visit Romania or would like
+to learn more about Romania.
+
+Authentic, Natural and Cultural are the words that best capture the essence of Romania,
+a dynamic country rich in history, arts and scenic beauty.
+
+Romania offers countless unique travel experiences that are waiting to be discovered.
+
+A journey of a few hours by car or train can take you from the Danube River to a beautiful, intact, medieval town in Transylvania; from Bucharest - Romania's capital city - to the
+Black Sea; from Southern Transylvania to Bucovina or Maramures. Take a step back in time
+as you visit one of the world's famous painted monasteries in Bucovina, the ancient, hilltop citadel in Sighisoara or an authentic, centuries-old, folkloric village in Maramures.</p>
+
+
+				</div><!--main col col-lg-8-->
+				<div class="sidebar col col-lg-5">
+					<h2>Upcoming Events</h2>
+					<!-- Nav tabs -->
+						<ul class="nav nav-tabs">
+							<li><a href="#mon" data-toggle="tab">Mon</a></li>
+							<li><a href="#tue" data-toggle="tab">Tue</a></li>
+							<li><a href="#wed" data-toggle="tab">Wed</a></li>
+							<li><a href="#thu" data-toggle="tab">Thu</a></li>
+							<li><a href="#fri" data-toggle="tab">Fri</a></li>
+							<li><a href="#sat" data-toggle="tab">Sat</a></li>
+							<li><a href="#sun" data-toggle="tab">Sun</a></li>
+						</ul><!--nav nav-tabs-->
+
+						<!-- Tab panes -->
+						<div class="tab-content">
+							<div class="tab-pane active" id="mon">
+								<ul>
+									<li>Lorem</li>
+									<li>ipsum</li>
+									<li>dolor</li>
+								</ul>
+							</div>
+							<div class="tab-pane" id="tue">
+								<ul>
+									<li>Lorem</li>
+									<li>ipsum</li>
+									<li>dolor</li>
+								</ul>
+							</div>
+							<div class="tab-pane" id="wed">
+								<ul>
+									<li>Lorem</li>
+									<li>ipsum</li>
+									<li>dolor</li>
+								</ul>
+							</div>
+							<div class="tab-pane" id="thu">
+								<ul>
+									<li>Lorem</li>
+									<li>ipsum</li>
+									<li>dolor</li>
+								</ul>
+							</div>
+							<div class="tab-pane" id="fri">
+								<ul>
+									<li>Lorem</li>
+									<li>ipsum</li>
+									<li>dolor</li>
+								</ul>
+							</div>
+							<div class="tab-pane" id="sat">
+								<ul>
+									<li>Lorem</li>
+									<li>ipsum</li>
+									<li>dolor</li>
+								</ul>
+							</div>
+							<div class="tab-pane" id="sun">
+								<ul>
+									<li>Lorem</li>
+									<li>ipsum</li>
+									<li>dolor</li>
+								</ul>
+							</div>
+						</div><!--tab-content-->
+
+					<aside class="photos">
+						<h2>Photos</h2>
+						<div class="modalphotos photogrid clearfix">
+							<img src="images/Ateneul_Roman_tn.jpg" alt="Ateneul Roman"/>
+							<img src="images/Brown_Bear_tn.jpg" alt="Brown Bear"/>
+							<img src="images/Mountain_City_tn.jpg" alt="Mountain City"/>
+							<img src="images/Constanta_Casino_tn.jpg" alt="Constanta Casino"/>
+							<img src="images/Corvin_Castle_tn.jpg" alt="Corvin Castle"/>
+							<img src="images/Sheep_Fagarasi_Mountain_tn.jpg" alt="Sheep Fagarasi Mountain"/>
+
+						</div>
+					</aside> <!--photos-->
+
+
+				</div><!--sidebar col col-lg-5-->
+			</section><!--content row-->
+
+
+			<section class="container">
+				<footer class="row">
+					<nav class="col-lg-12">
+
+						<ul class="breadcrumb">
+							<li><a href="#">Feedback Us</a></li>
+							<li><a href="#">Site Map</a></li>
+							<li><a href="#">Privacy Policy</a></li>
+						</ul>
+
+					</nav><!--col-lg-12-->
+				</footer><!--row-->
+			</section><!--container-->
+
+		</div><!--container-->
+
+		<section id="modal" class="modal fade" >
+			<div class="modal-body">
+				<img id="modalimage" src="" alt="Modal Photo" >
+			</div><!--modal-body-->
+		</section><!--modal fade-->
+
+
+		<script src="files/js/bootstrap.js"></script>
+
+		<script src="files/js/myscrypt.js"></script>
+	</body><!--home-->
+</html>
